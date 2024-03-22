cdd7bbe58a854c79a2229363305e18f60f0a0295
itwill <itwill@DESKTOP-5857598>
2024-03-22 13:53:13 +0900

    /

--- lab04_array/src/com/itwill/array08/ArrayMain08.java
+++ lab04_array/src/com/itwill/array08/ArrayMain08.java
@@ -12,12 +12,32 @@ public class ArrayMain08 {
 
 		// (1) 2차원 배열을 생성할 때 행과 열의 개수가 모두 결정된 경우 -> 직사각형 모양 배열
 		int array1[][] = new int[2][3];
-		for (int[] is : array1) {
-			for (int is2 : is) {
-				System.out.printf("%d\t", is2);
+
+		for (int row[] : array1) {
+			for (int column : row) {
+				System.out.printf("%d\t", column);
 			}
 			System.out.println();
 		}
+		System.out.println("====================================");
+		// (2) 2차원 배열의 행의 개수는 결정되어져 있고, 열의 개수가 결정되지 않은 경우:
+		// 2차원 배열이 가지고 있는 1차원 배열의 개수는 알고 있지만,
+		// 각각의 1차원 배열들의 원소 개수는 아직 결정되지 않은 경우:
+		int arrary2[][] = new int[3][]; // 1차원 배열 3개를 원소로 갖는 2차원 배열
+
+		// 열의 개수가 미정인 2차원 배열은 사용하기 전에 반드시 1차원 배열들을 각각 생성해야만 함
+		arrary2[0] = new int[2]; // 원소 2개를 갖는 1차원 배열
+		arrary2[1] = new int[4]; // 원소 4개를 갖는 1차원 배열
+		arrary2[2] = new int[3]; // 원소 3개를 갖는 1차원 배열
+
+		for (int row[] : arrary2) {
+			for (int column : row) {
+				column = 145;
+				System.out.printf("%d\t", column);
+			}
+			System.out.println();
+		}
+
 	}
 
 }
