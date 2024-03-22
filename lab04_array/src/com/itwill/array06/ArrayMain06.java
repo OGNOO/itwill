f79552df8b84f4a556cf9ed4c77ec7eabe9654ff
itwill <itwill@DESKTOP-5857598>
2024-03-22 10:41:33 +0900

    itwill

---
+++ lab04_array/src/com/itwill/array06/ArrayMain06.java
@@ -0,0 +1,13 @@
+package com.itwill.array06;
+
+public class ArrayMain06 {
+
+	public static void main(String[] args) {
+		// 다차원 배열: 배열들을 원소로 갖는 배열
+		// 2차원 배열: 1차원 배열들을 원소로 갖는 배열
+		// - 2차원 배열은 인덱스를 2개를 갖음
+		
+
+	}
+
+}
