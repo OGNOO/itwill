3adcf1caa505aaeeea4bb798e2bae05086213fd6
itwill <itwill@DESKTOP-5857598>
2024-04-05 18:17:41 +0900

    /

--- lab11_collection/src/com/itwill/list04/ListMain04.java
+++ lab11_collection/src/com/itwill/list04/ListMain04.java
@@ -11,7 +11,6 @@ public class ListMain04 {
 
 		// 1. names 에서 5글자 이상인 문자열을 대문자로 변환해서 저장하는 ArrayList를 만들고 출력.
 		// -> [JAVASCRIPT, SERVLET, SPRING]
-
 		ArrayList<String> arr1 = new ArrayList<>();
 
 		for (int i = 0; i < names.size(); i++) {
@@ -23,7 +22,6 @@ public class ListMain04 {
 
 		// 2. names 에 저장된 문자열의 글자수들을 저장하는 ArrayList를 만들고 출력.
 		// -> [4, 3, 4, 3, 10, 7, 3, 6]
-
 		ArrayList<Integer> arr2 = new ArrayList<>();
 
 		for (int i = 0; i < names.size(); i++) {
@@ -31,9 +29,9 @@ public class ListMain04 {
 		}
 		System.out.println(arr2);
 
+		// 3. codes 의 원소가 0이면 "남성", 1이면 "여성"을 저장하는 ArrayList를 만들고 출력.
 		List<Integer> codes = Arrays.asList(0, 1, 0, 1, 1, 0);
 		ArrayList<String> arr3 = new ArrayList<>();
-		// 3. codes 의 원소가 0이면 "남성", 1이면 "여성"을 저장하는 ArrayList를 만들고 출력.
 
 		for (int i = 0; i < codes.size(); i++) {
 			if (codes.get(i) == 1) {
@@ -44,5 +42,4 @@ public class ListMain04 {
 		}
 		System.out.println(arr3);
 	}
-
 }
